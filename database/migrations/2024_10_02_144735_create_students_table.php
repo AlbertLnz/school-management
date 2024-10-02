9d9b3e3851bb7d48e998a7d61c16b3291f664918
Albert Lanza <albert2000_lanza@hotmail.com>
2024-10-02 16:57:25 +0200

    StudentController, Migration & Factory

---
+++ database/migrations/2024_10_02_144735_create_students_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('surname');
+            $table->integer('age');
+
+            $table->foreignId('class_id')->constrained('classes');
+            $table->foreignId('student_subject_id')->constrained('student_subject');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
