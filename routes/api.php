b290863f264c62253f7e14ea2327003ffb3caefb
Albert Lanza <albert2000_lanza@hotmail.com>
2024-10-03 09:04:39 +0200

    Testing the api (works fine)

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/test', [StudentController::class, 'index'])->name('api.test');
