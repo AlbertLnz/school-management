d4407c42b3a37a9678b521c7ea5f63857cdd2e66
Albert Lanza <albert2000_lanza@hotmail.com>
2024-10-03 10:01:22 +0200

    Subject CRUD did it w/o authentification

---
+++ app/Http/Controllers/SubjectController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Subject;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+// Command: php artisan make:controller SubjectController --resource
+class SubjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $subjects = Subject::all();
+
+        return response()->json($subjects);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        $validator = Validator::make($data, [
+            'name' => 'required | min:3  | max:20',
+        ]);
+
+        if ($validator->fails()) {
+            return response(['error' => $validator->errors()], 401);
+        } else {
+
+            $subject = new Subject();
+            $subject->name = $data['name'];
+
+            $subject->save();
+
+            return response([
+                'message' => 'New subject created!',
+                'subject created' => response()->json($subject)
+            ], 200);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $subject = Subject::find($id);
+
+        if (is_null($subject)) {
+            return response()->json(['message' => 'Subject not found'], 404);
+        }
+
+        return response()->json($subject);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $subject = Subject::find($id);
+
+        if (is_null($subject)) {
+            return response()->json(['message' => 'Subject not found'], 404);
+        }
+
+        $data = $request->all();
+
+        $validator = Validator::make($data, [
+            'name' => 'required | min:3  | max:20',
+        ]);
+
+        if ($validator->fails()) {
+            return response(['error' => $validator->errors()], 401);
+        } else {
+            $subject->fill($data);
+            $subject->save($data);
+
+            return response([
+                'message' => 'Subject updated!',
+                'subject updated' => response()->json($subject)
+            ], 200);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $subject = Subject::find($id);
+
+        if (is_null($subject)) {
+            return response()->json(['message' => 'Subject not found'], 404);
+        } else {
+            $subject->delete();
+
+            return response([
+                'message' => "Subject deleted!",
+                'subject deleted' => $subject
+            ], 200);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\SubjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,12 @@ Route::put('/students/{id}', [StudentController::class, 'update'])->name('api.st
 Route::delete('/students/{id}', [StudentController::class, 'destroy'])->name('api.students.destroy'); // D
 
 Route::get('/students/{id}', [StudentController::class, 'show'])->name('api.students.show'); // Get 1 student
+
+
+// Subjects CRUD
+Route::post('/subject', [SubjectController::class, 'store'])->name('api.subjects.store'); // C
+Route::get('/subjects', [SubjectController::class, 'index'])->name('api.subjects.index'); // R
+Route::put('/subjects/{id}', [SubjectController::class, 'update'])->name('api.subjects.update'); // U
+Route::delete('/subjects/{id}', [SubjectController::class, 'destroy'])->name('api.subjects.destroy'); // D
+
+Route::get('/subjects/{id}', [SubjectController::class, 'show'])->name('api.subjects.show'); // Get 1 subject
