f53307e9060a8aaadbae96b3423391ba3aea142d
Albert Lanza <albert2000_lanza@hotmail.com>
2024-10-05 20:30:08 +0200

    Temporary redirect from '/' to '/dashboard'

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,10 @@
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::redirect('/', '/dashboard', 307); # Temporary redirect (login page)
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('web.dashboard');
 Route::get('/dashboard/students/{id}', [DashboardController::class, 'index2'])->name('web.dashboard2');
